610e3921633f94adb003ee4e79b08dd22d0e0a2b
Amalia <amaliaponcetoledo@gmail.com>
2023-06-20 21:43:11 +0200

    Nuevo estilo css

--- Js/timeline.js
+++ Js/timeline.js
@@ -1,6 +1,3 @@
-'use strict';
-
-
 fetch('zelda-timeline.json')
   .then(response => response.json())
   .then(data => {
@@ -15,30 +12,38 @@ fetch('zelda-timeline.json')
 
     // Loop through the sorted data and create HTML elements for each event
     data.forEach(event => {
-        // Create the elements
-        const eventElement = document.createElement('div');
-        const titleElement = document.createElement('h2');
-        const imageElement = document.createElement('img');
-        const textElement = document.createElement('p');
-    
-        // Set the content for each element
-        titleElement.textContent = event.title;
-        imageElement.src = event.image;
-        textElement.textContent = event.text;
-    
-        // Add the "timeline-event" class to the event element
-        eventElement.classList.add('timeline-event');
-        // Set the data-year attribute to the event element
-        eventElement.setAttribute('data-year', event.date);
-    
-        // Append the elements to the event container
-        eventElement.appendChild(titleElement);
-        eventElement.appendChild(imageElement);
-        eventElement.appendChild(textElement);
-    
-        // Append the event container to the timeline container
-        timelineContainer.appendChild(eventElement);
+      // Create the elements
+      const eventElement = document.createElement('div');
+      const contentWrapper = document.createElement('div');
+      const titleElement = document.createElement('h2');
+      const imageElement = document.createElement('img');
+      const textElement = document.createElement('p');
+
+      // Set the content for each element
+      titleElement.textContent = event.title;
+      imageElement.src = event.image;
+      textElement.textContent = event.text;
+
+      // Add classes to the elements
+      eventElement.classList.add('timeline-event');
+      contentWrapper.classList.add('event-content-wrapper');
+      titleElement.classList.add('event-title');
+      imageElement.classList.add('event-image');
+      textElement.classList.add('event-text');
+
+      // Append the elements to the content wrapper
+      contentWrapper.appendChild(titleElement);
+      contentWrapper.appendChild(imageElement);
+      contentWrapper.appendChild(textElement);
+
+      // Append the content wrapper to the event element
+      eventElement.appendChild(contentWrapper);
+
+      // Append the event element to the timeline container
+      timelineContainer.appendChild(eventElement);
     });
+
+   
   })
   .catch(error => {
     console.error('Error fetching the JSON dataset:', error);
