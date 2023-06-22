3ff4324e6d6b41369d2140a24ca8bf8b9036b5af
Amalia <amaliaponcetoledo@gmail.com>
2023-06-22 21:44:55 +0200

    Form

--- Js/timeline.js
+++ Js/timeline.js
@@ -12,35 +12,93 @@ fetch('zelda-timeline.json')
 
     // Loop through the sorted data and create HTML elements for each event
     data.forEach(event => {
-      // Create the elements
+      // Creando elementos (DOM)
       const eventElement = document.createElement('div');
       const contentWrapper = document.createElement('div');
       const titleElement = document.createElement('h2');
       const imageElement = document.createElement('img');
       const textElement = document.createElement('p');
 
-      // Set the content for each element
+      // Contenido para cada elemento (DOM)
       titleElement.textContent = event.title;
       imageElement.src = event.image;
       textElement.textContent = event.text;
 
-      // Add classes to the elements
+      // Añadir clases al elemento (DOM)
       eventElement.classList.add('timeline-event');
       contentWrapper.classList.add('event-content-wrapper');
       titleElement.classList.add('event-title');
       imageElement.classList.add('event-image');
       textElement.classList.add('event-text');
 
-      // Append the elements to the content wrapper
+      // Agregando elementos
       contentWrapper.appendChild(titleElement);
       contentWrapper.appendChild(imageElement);
       contentWrapper.appendChild(textElement);
 
-      // Append the content wrapper to the event element
+      //Agregando contenido
       eventElement.appendChild(contentWrapper);
 
-      // Append the event element to the timeline container
+      // Agregando elemento del evento al contenedor timeline
       timelineContainer.appendChild(eventElement);
+      // Función para obtener los eventos almacenados en el almacenamiento local
+
+const obtenerEventosAlmacenados = () => {
+  const eventosGuardados = localStorage.getItem('eventos');
+  return eventosGuardados ? JSON.parse(eventosGuardados) : [];
+};
+
+// Función para guardar los eventos en el almacenamiento local
+const guardarEventos = (eventos) => {
+  localStorage.setItem('eventos', JSON.stringify(eventos));
+};
+
+// Cargar los eventos almacenados en el timeline al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+  const eventosAlmacenados = obtenerEventosAlmacenados();
+  const timelineContainer = document.getElementById('timeline-container');
+
+  eventosAlmacenados.forEach(evento => {
+    agregarEventoAlTimeline(evento);
+  });
+});
+
+// Agregar un event listener para la presentación del formulario
+const formulario = document.getElementById('formulario');
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Obtener los valores de entrada del formulario
+
+  const titulo = document.getElementById('event-title').value;
+  const fecha = document.getElementById('event-date').value;
+  const imagen = document.getElementById('event-image').value;
+  const descripcion = document.getElementById('event-description').value;
+
+  // Crear un nuevo objeto de evento
+  const nuevoEvento = {
+    title: titulo,
+    date: parseInt(fecha),
+    image: imagen,
+    text: descripcion
+  };
+
+  // Agregar el nuevo evento al timeline
+
+  agregarEventoAlTimeline(nuevoEvento);
+
+  // Obtener los eventos almacenados y agregar el nuevo evento
+  
+  const eventosAlmacenados = obtenerEventosAlmacenados();
+  eventosAlmacenados.push(nuevoEvento);
+
+  // Guardar los eventos actualizados en el almacenamiento local
+  guardarEventos(eventosAlmacenados);
+
+  // Limpiar los campos del formulario
+  formulario.reset();
+});
+
     });
 
    
